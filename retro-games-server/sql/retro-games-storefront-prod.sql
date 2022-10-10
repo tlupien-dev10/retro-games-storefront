985ee16818fe14b03ad22053634305fda56e8375
Tyler Bencs <tbencs@dev-10.com>
2022-10-10 11:15:55 -0500

    Finished Production SQL

--- retro-games-server/sql/retro-games-storefront-prod.sql
+++ retro-games-server/sql/retro-games-storefront-prod.sql
@@ -37,14 +37,81 @@ create table listing (
     quantity int not null 
 );
 
+create table merchandise (
+	merchandise_id int primary key auto_increment,
+    merchandise_name varchar(100),
+    listing_id int not null,
+    constraint fk_merchandise_listing_id
+		foreign key (listing_id)
+        references listing(listing_id)
+);
+
 create table game (
 	game_id int primary key auto_increment,
     genre varchar(50) not null,
     publisher varchar(50) not null,
     release_date date not null,
+    listing_id int not null,
     constraint fk_game_listing_id
 		foreign key (listing_id)
         references listing(listing_id)
 );
+
+create table console (
+	console_id int primary key auto_increment,
+    console_version varchar(50) not null,
+    company varchar(50) not null,
+    console_release_date date not null,
+    listing_id int not null,
+    constraint fk_console_listing_id
+		foreign key (listing_id)
+        references listing(listing_id)
+);
+
+create table game_console (
+	game_id int not null,
+    console_id int not null,
+    constraint pk_game_console
+		primary key (game_id, console_id),
+	constraint fk_game_console_game_id
+		foreign key (game_id)
+        references game(game_id),
+	constraint fk_game_console_console_id
+		foreign key (console_id)
+        references console(console_id)
+);
+
+create table review (
+	review_id int primary key auto_increment,
+    review_description varchar(200),
+    listing_id int not null,
+    constraint fk_review_listing_id
+		foreign key (listing_id)
+        references listing(listing_id)
+);
+
+create table `order` (
+	order_id int primary key auto_increment,
+    total numeric(8, 2),
+    app_user_id int not null,
+    constraint fk_order_app_user_id
+		foreign key (app_user_id)
+        references app_user(app_user_id)
+);
+
+create table order_listing (
+	order_id int not null,
+    listing_id int not null,
+    constraint pk_order_listing
+		primary key (order_id, listing_id),
+	constraint fk_order_listing_order_id
+		foreign key (order_id)
+        references `order`(order_id),
+	constraint fk_order_listing_listing_id
+		foreign key (listing_id)
+        references listing(listing_id)
+);
+
+
 
         
