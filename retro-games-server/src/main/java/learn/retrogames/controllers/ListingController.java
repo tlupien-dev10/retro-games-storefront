cfd490559b97a1d4b3d95c20737f81edb3d9f721
Tim Lupien <tlupien@dev-10.com>
2022-10-11 11:41:15 -0500

    changed listing request mapping

--- retro-games-server/src/main/java/learn/retrogames/controllers/ListingController.java
+++ retro-games-server/src/main/java/learn/retrogames/controllers/ListingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
-@RequestMapping("/api")
+@RequestMapping("/api/listing")
 public class ListingController {
     private final ListingService service;
 
