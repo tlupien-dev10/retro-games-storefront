8714f41f48a8b53436cbc85eea43b1e9142a1e99
Tyler Bencs <tbencs@dev-10.com>
2022-10-12 14:53:38 -0500

    Finished Order Controller

--- retro-games-server/src/main/java/learn/retrogames/controllers/OrderController.java
+++ retro-games-server/src/main/java/learn/retrogames/controllers/OrderController.java
@@ -1,14 +1,70 @@
 package learn.retrogames.controllers;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import learn.retrogames.domain.OrderService;
+import learn.retrogames.domain.Result;
+import learn.retrogames.models.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/order")
 public class OrderController {
+
+    private final OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Order> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{orderId}")
+    public Order getById(@PathVariable int orderId) {
+        return service.getById(orderId);
+    }
+
+    @PostMapping("/{orderId}")
+    public ResponseEntity<Object> add(@RequestBody Order order){
+        Result<Order> result = service.add(order);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<Object> update(@PathVariable int orderId, @RequestBody Order order){
+        if (orderId != order.getId()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Order> result = service.update(order);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int orderId){
+        if (service.deleteById(orderId)){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
 
 
 
--- retro-games-server/src/main/java/learn/retrogames/domain/OrderService.java
+++ retro-games-server/src/main/java/learn/retrogames/domain/OrderService.java
@@ -1,10 +1,31 @@
 package learn.retrogames.domain;
 
+import learn.retrogames.models.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
 
-    
 
+    public List<Order> getAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Order getById(int orderId) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Result<Order> add(Order order) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Result<Order> update(Order order) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean deleteById(int orderId) {
+        throw new UnsupportedOperationException();
+    }
 }
