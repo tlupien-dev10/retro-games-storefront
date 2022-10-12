516dfe3f0f8759198725ce0f82a6a17004fc76e1
Tyler Bencs <tbencs@dev-10.com>
2022-10-12 11:57:31 -0500

    Finished Listing Controller

---
+++ retro-games-server/src/main/java/learn/retrogames/controllers/ErrorResponse.java
@@ -0,0 +1,24 @@
+package learn.retrogames.controllers;
+
+
+import learn.retrogames.domain.Result;
+import learn.retrogames.domain.ResultType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ErrorResponse {
+
+    public static <T>ResponseEntity<Object> build(Result<T> result){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        if (result.getType() == null || result.getType() == ResultType.INVALID){
+            status = HttpStatus.BAD_REQUEST;
+        } else if (result.getType() == ResultType.NOT_FOUND){
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(result.getMessages(), status);
+
+    }
+
+
+}
--- retro-games-server/src/main/java/learn/retrogames/controllers/ListingController.java
+++ retro-games-server/src/main/java/learn/retrogames/controllers/ListingController.java
@@ -1,11 +1,11 @@
 package learn.retrogames.controllers;
 
 import learn.retrogames.domain.ListingService;
+import learn.retrogames.domain.Result;
 import learn.retrogames.models.Listing;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +23,42 @@ public class ListingController {
     public List<Listing> getAll() {
         return service.getAll();
     }
+
+    @GetMapping("/{listingId}")
+    public Listing getById(@PathVariable int listingId){
+        return service.getById(listingId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Listing listing){
+        Result<Listing> result = service.add(listing);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{listingId}")
+    public ResponseEntity<Object> update(@PathVariable int listingId, @RequestBody Listing listing){
+        if (listingId != listing.getId()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Listing> result = service.update(listing);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{listingId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int listingId) {
+        if (service.deleteById(listingId)){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
