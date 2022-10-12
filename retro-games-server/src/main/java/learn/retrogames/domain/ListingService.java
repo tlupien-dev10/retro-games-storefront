36693773b8baefff33e0b6adfb521928e1fb0bc8
Tim Lupien <tlupien@dev-10.com>
2022-10-12 10:40:40 -0500

    worked on listing service

--- retro-games-server/src/main/java/learn/retrogames/domain/ListingService.java
+++ retro-games-server/src/main/java/learn/retrogames/domain/ListingService.java
@@ -33,4 +33,52 @@ public class ListingService {
     // - must have an image path
     //
 
+    private Result<Listing> validate (Listing listing) {
+        Result<Listing> res = new Result<>();
+        if (listing == null) {
+            res.addMessage("Listing cannot be null.", ResultType.INVALID);
+        }
+
+        if (listing.getName() == null) {
+            res.addMessage("Listing must have a name.", ResultType.INVALID);
+        }
+
+        if (listing.getPrice() == null || listing.getPrice().signum() < 0) {
+            res.addMessage("Price must exist and be positive.", ResultType.INVALID);
+        }
+
+        if (listing.getQuantity() < 0 ) {
+            res.addMessage("Quantity must be positive or 0.", ResultType.INVALID);
+        }
+
+        if (listing.getListingType() == null) {
+            res.addMessage("Listing must have a listing type.", ResultType.INVALID);
+        }
+
+        switch (listing.getListingType()) {
+            case GAME:
+                if (listing.getGame() == null) {
+                    res.addMessage("Game listings must have game details associated with them.", ResultType.INVALID);
+                }
+                if (listing.getConsole() != null || listing.getMerchandise() != null) {
+                    res.addMessage("Game listings may not have console or merchandise details.", ResultType.INVALID);
+                }
+            case CONSOLE:
+                if (listing.getConsole() == null) {
+                    res.addMessage("Console listings must have console details associated with them.", ResultType.INVALID);
+                }
+                if (listing.getGame() != null || listing.getMerchandise() != null) {
+                    res.addMessage("Console listings may not have game or merchandise details.", ResultType.INVALID);
+                }
+            case MERCHANDISE:
+                if (listing.getMerchandise() == null) {
+                    res.addMessage("Merchandise listings must have merchandise details associated with them.", ResultType.INVALID);
+                }
+                if (listing.getGame() != null || listing.getConsole() != null) {
+                    res.addMessage("Merchandise listings may not have game or console details.", ResultType.INVALID);
+                }
+        }
+
+        return res;
+    }
 }
