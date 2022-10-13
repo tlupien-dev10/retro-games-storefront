aa09863aab44c6d1e6b997c92a9d1bdbd39bda9b
Tim Lupien <tlupien@dev-10.com>
2022-10-13 12:01:27 -0500

    added some todos in order vertical

--- retro-games-server/src/main/java/learn/retrogames/data/OrderJdbcTemplateRepository.java
+++ retro-games-server/src/main/java/learn/retrogames/data/OrderJdbcTemplateRepository.java
@@ -100,6 +100,7 @@ public class OrderJdbcTemplateRepository  implements OrderRepository{
 
     private void getCustomerForOrder(Order order) {
         final String sql = "SELECT * FROM app_user WHERE app_user_id = ?;";
+        // TODO: get the customer with a subquery based on the order id
         AppUser customer = jdbcTemplate.query(sql, new AppUserMapperLite(), order.getCustomer().getAppUserId()).stream()
                 .findFirst()
                 .orElse(null);
--- retro-games-server/src/main/java/learn/retrogames/domain/OrderService.java
+++ retro-games-server/src/main/java/learn/retrogames/domain/OrderService.java
@@ -58,18 +58,22 @@ public class OrderService {
         }
         if (order.getListings() == null || order.getListings().size() == 0) {
             res.addMessage("Orders must have products associated with them.", ResultType.INVALID);
-        } else if (order.getListings().stream().map(Listing::getOrderedQuantity).map(qty -> qty > 0).collect(Collectors.toList()).contains(false)) {
-            res.addMessage("You may not order 0 of a product.", ResultType.INVALID);
-        }
+        } else {
 
-        List<Listing> listings = order.getListings();
-        for (Listing l : listings) {
-            if (l.getOrderedQuantity() > l.getQuantity()) {
-                res.addMessage(
-                        String.format("There are only %s %s in stock. Orders for an item cannot exceed what is in stock.",
-                                l.getQuantity(),
-                                l.getName()),
-                        ResultType.INVALID);
+            if (order.getListings().stream().map(Listing::getOrderedQuantity).map(qty -> qty > 0).collect(Collectors.toList()).contains(false)) {
+                res.addMessage("You may not order 0 of a product.", ResultType.INVALID);
+            }
+
+            List<Listing> listings = order.getListings();
+            for (Listing l : listings) {
+                if (l.getOrderedQuantity() > l.getQuantity()) {
+                    res.addMessage(
+                            String.format("There are only %s %s in stock. Orders for an item cannot exceed what is in stock.",
+                                    l.getQuantity(),
+                                    l.getName()),
+                            ResultType.INVALID);
+                }
+                // TODO: check if listings exist here
             }
         }
         return res;
