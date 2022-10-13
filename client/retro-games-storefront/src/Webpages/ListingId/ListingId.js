178deaf91ea0b69c1360cf91b1fd302e0eea0666
Tim Lupien <tlupien@dev-10.com>
2022-10-13 15:28:32 -0500

    fix to listing details page

--- client/retro-games-storefront/src/Webpages/ListingId/ListingId.js
+++ client/retro-games-storefront/src/Webpages/ListingId/ListingId.js
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
 import "./ListingId.css";
-import ListingIdDisplay from "./ListingIdDisplay";
 import useAuth from "../../Components/Hooks/useAuth";
 import PageErrors from "../../Components/PageErrors/PageErrors";
 
 function ListingId() {
-  const [listing, setListing] = useState([]);
+  const [listing, setListing] = useState({reviews:[]});
   const [error, setError] = useState([]);
   const {id} = useParams();
 
@@ -17,7 +16,11 @@ function ListingId() {
   function getListing() {
     fetch("http://localhost:8080/api/listing/" + id)
         .then((response) => response.json())
-        .then((data) => setListing(data))
+        .then((data) => {
+          // console.log(data);
+          // console.log(data.reviews);
+          return setListing(data);
+        })
         .catch((err) => setError([...err]));
 
 }
@@ -27,9 +30,23 @@ useEffect(() => getListing(), []);
   return (
     <div>
       <PageErrors errors={error} />
-      {listing.map((listings) => (
-        <ListingIdDisplay key={listings.id} listing={listings} />
-      ))}
+      <div>
+      <figure>
+        {"../../"+ listing.imagePath && (
+          <img
+            className="card-img-top"
+            src={"../../"+ listing.imagePath}
+            alt={listing.description}
+          />
+        )}
+        <figcaption>
+          <h4>{listing.description}</h4>
+          <p>Name: {listing.name}</p>
+          <p>Price: {listing.price}</p>
+        </figcaption>
+      </figure>
+       {listing.reviews.map(r => <p key={r.id}>{r.title}</p>)}
+    </div>
     </div>
   );
 }
