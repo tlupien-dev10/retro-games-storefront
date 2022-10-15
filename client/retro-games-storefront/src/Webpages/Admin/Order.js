0836b80e44c839ad49af5ab9ea8a304a2dafe1e4
Jake Frisch <Fjrisch@gmail.com>
2022-10-15 14:23:12 -0500

    Working on formatting

--- client/retro-games-storefront/src/Webpages/Admin/Order.js
+++ client/retro-games-storefront/src/Webpages/Admin/Order.js
@@ -5,18 +5,18 @@ function Order({order, deleteOrder}){
     return (
         <div className="row">
           <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className="card-title">Username: {order.customer.username}</span>
-                Listing: {order.listings.map((listing) => (
+            <div className="card blue-grey darken-1" id="orderCard">
+              <div className="card-content white-text" id="orderData">
+                <span className="card-title" id="orderTitle">Username: {order.customer.username}</span>
+                Purchase Summary: {order.listings.map((listing) => (
                     <>
                     <p>{listing.name}</p>
                     <p>Qty: {listing.orderedQuantity}</p>
-                    <p>Price:{listing.price}</p>
+                    <p>Price: ${listing.price}</p>
                     </>
                     
                 ))}
-                <p>Order Total:{order.listings.map((listing) => (
+                <p>Order Total: ${order.listings.map((listing) => (
                     listing.orderedQuantity*listing.price)).reduce((a, b) => a + b, 0)}
                 </p>
               </div>
