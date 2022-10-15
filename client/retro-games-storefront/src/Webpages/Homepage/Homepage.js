0884dc6bf01ee35f236fb13c1637c40ea393351f
Jake Frisch <Fjrisch@gmail.com>
2022-10-15 13:26:04 -0500

    Fixing Navigation

--- client/retro-games-storefront/src/Webpages/Homepage/Homepage.js
+++ client/retro-games-storefront/src/Webpages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 // import Carousel from "./Webpages/Homepage/Carousel";
-import Carousel2 from "../../Components/Carousel2";
+import Carousel2 from "../../Components/Carousel/Carousel2";
 import "./Homepage.css";
 
 import { Link } from "react-router-dom";
