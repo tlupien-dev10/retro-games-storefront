a10f5da5f245b812710d131883e9002e43b188d7
Jake Frisch <Fjrisch@gmail.com>
2022-10-15 15:49:39 -0500

    Working on CSS

--- client/retro-games-storefront/src/Webpages/Homepage/Homepage.js
+++ client/retro-games-storefront/src/Webpages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 // import Carousel from "./Webpages/Homepage/Carousel";
-import Carousel2 from "../../Components/Carousel/Carousel2";
+import Carousel2 from "../../Components/./Carousel/Carousel2";
 import "./Homepage.css";
 
 import { Link } from "react-router-dom";
@@ -19,9 +19,8 @@ function Homepage() {
       <Carousel2 />
 
       <Link to={"/listing"}>
-        <button className="btn btn-sm btn-success" id="homeBtn">
-          Enter to begin shopping
-        </button>
+        <button className="btn btn-sm btn-success" id="homeBtn">i class="material-icons">shopping_cart</i>
+          Enter to begin shopping</button>
       </Link>
     </div>
 
