1d11417443ff603cd6664e4df62b6f09c365a6ef
Jake Frisch <Fjrisch@gmail.com>
2022-10-15 14:49:07 -0500

    Pushing

--- client/retro-games-storefront/src/Webpages/Admin/Order.js
+++ client/retro-games-storefront/src/Webpages/Admin/Order.js
@@ -5,8 +5,8 @@ function Order({order, deleteOrder}){
     return (
         <div className="row">
           <div className="col s12 m6">
-            <div className="card blue-grey darken-1" id="orderCard">
-              <div className="card-content white-text" id="orderData">
+            <div className="card" id="orderCard">
+              <div className="card-content" id="orderData">
                 <span className="card-title" id="orderTitle">Username: {order.customer.username}</span>
                 Purchase Summary: {order.listings.map((listing) => (
                     <>
@@ -19,9 +19,9 @@ function Order({order, deleteOrder}){
                 <p>Order Total: ${order.listings.map((listing) => (
                     listing.orderedQuantity*listing.price)).reduce((a, b) => a + b, 0)}
                 </p>
-              </div>
-              <div className="card-action">
-                <button onClick={() => deleteOrder(order.id)}>Delete</button>
+         
+                <button id="orderDelete" onClick={() => deleteOrder(order.id)}>< i class="material-icons">clear</i></button>
+        
               </div>
             </div>
           </div>
