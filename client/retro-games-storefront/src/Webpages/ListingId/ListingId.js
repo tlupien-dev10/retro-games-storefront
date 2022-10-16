1a62d6eeb7270f5b345193d4ec5440e718d54e91
Jake Frisch <Fjrisch@gmail.com>
2022-10-16 18:07:06 -0500

    Conditional Display all relevant data for Console, and Merchandise in Listing/ID. Game still needs work to display Console name. Displays others

--- client/retro-games-storefront/src/Webpages/ListingId/ListingId.js
+++ client/retro-games-storefront/src/Webpages/ListingId/ListingId.js
@@ -6,50 +6,102 @@ import useAuth from "../../Components/Hooks/useAuth";
 import PageErrors from "../../Components/PageErrors/PageErrors";
 
 function ListingId() {
-  const [listing, setListing] = useState({reviews:[]});
+  const [listing, setListing] = useState({ reviews: [] });
   const [error, setError] = useState([]);
-  const {id} = useParams();
+  const { id } = useParams();
+  const [game, setGame] = useState([]);
+  const [consoles, setConsoles] = useState([]);
 
   const auth = useAuth();
   const history = useHistory();
 
   function getListing() {
     fetch("http://localhost:8080/api/listing/" + id)
-        .then((response) => response.json())
-        .then((data) => {
-          // console.log(data);
-          // console.log(data.reviews);
-          return setListing(data);
-        })
-        .catch((err) => setError([...err]));
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        // console.log(data.reviews);
+        return setListing(data);
+      })
+      .catch((err) => setError([...err]));
+  }
 
-}
+  useEffect(() => getListing(), []);
 
-useEffect(() => getListing(), []);
+  function getGame() {
+    fetch("http://localhost:8080/api/listing")
+      .then((response) => response.json())
+      .then((data) => {
+        return setGame(data);
+      })
+      .catch((err) => setError([...err]));
+  }
+
+  useEffect(() => getGame(), []);
+
+  function getConsoles() {
+    fetch("http://localhost:8080/api/listing")
+      .then((response) => response.json())
+      .then((data) => {
+        setConsoles(data.filter(l => l.console));
+      })
+      .catch((err) => setError([...err]));
+  }
+
+useEffect(() => getConsoles(), []);
 
   return (
     <div>
       <PageErrors errors={error} />
       <div>
-      <figure>
-        {"../../"+ listing.imagePath && (
-          <img
-            className="card-img-top" id="listingIdImg"
-            src={"../../"+ listing.imagePath}
-            alt={""}
-          />
-        )}
-        <figcaption id="listingIdText">
-          <h4>{listing.name}</h4>
-          <p>Description: {listing.description}</p>
-          <p id="listingIdPrice">Price: ${listing.price}</p>
-        </figcaption>
-      </figure>
-      <div id="reviews">
-        <h5>Reviews</h5>
-       {listing.reviews.map(r => <p key={r.id}>{r.title}</p>)}
-       </div>
-    </div>
+        <figure>
+          {"../../" + listing.imagePath && (
+            <img
+              className="card-img-top"
+              id="listingIdImg"
+              src={"../../" + listing.imagePath}
+              alt={""}
+            />
+          )}
+          <figcaption id="listingIdText">
+            <h4>{listing.name}</h4>
+            <p>Type: {listing.listingType}</p>
+            <p>Description: {listing.description}</p>
+            <p id="listingIdPrice">Price: ${listing.price}</p>
+            {listing.listingType === "GAME" ? (
+              <div> {console.log(listing.game)}Consoles: {listing.game.consoles.map(c => <p key={c.console.id}>{c.name}</p>)}
+                Genre: {listing.game.genre} <br></br>Publisher:{" "} 
+                {listing.game.publisher}
+                <br></br>Release Date: {listing.game.releaseDate}
+              </div>
+            ) : (
+              <></>
+            )}
+            {listing.listingType === "CONSOLE" ? (
+              <p>
+                Version: {listing.console.version} <br></br>Company:{" "}
+                {listing.console.company}
+                <br></br>Release Date: {listing.console.releaseDate}
+              </p>
+            ) : (
+              <></>
+            )}
+            {listing.listingType === "MERCHANDISE" ? (
+              <p>
+                Category: {listing.merchandise.category}
+              </p>
+            ) : (
+              <></>
+            )}
+          </figcaption>
+        </figure>
+        <div id="reviews">
+          <h5>Reviews</h5>
+          {listing.reviews.map((r) => (
+            <p key={r.id}>{r.title}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
