c03afdaf0ae34d155bbc42d8ec913ff1be40382d
Tim Lupien <tlupien@dev-10.com>
2022-10-17 11:00:06 -0500

    started filtration

---
+++ client/retro-games-storefront/src/Webpages/Listing/FilterForm.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import './FilterForm.css'
+
+const CLEAR_FORM = {
+    gameCheckbox: false
+}
+
+function FilterForm({filter}) {
+
+    const [filterObject, setFilterObject] = useState(CLEAR_FORM)
+
+    const changeHandler = function(evt) {
+        const newFilterObject = {...filterObject};
+        if (evt.target.type === "checkbox") {
+            newFilterObject[evt.target.name] = evt.target.checked;
+        }
+        setFilterObject(newFilterObject);
+    }
+
+    return (
+    <form onSubmit={(evt) => filter(evt, filterObject)}>
+        <fieldset id="checks">
+        <label>
+            <input type="checkbox" id="gameCheckbox" name="gameCheckbox" checked={filterObject.gameCheckbox} onChange={changeHandler} />
+            <span>Game</span>
+        </label>
+        </fieldset>
+        <button>Apply</button>
+    </form>
+        );
+}
+
+export default FilterForm;
--- client/retro-games-storefront/src/Webpages/Listing/Listings.js
+++ client/retro-games-storefront/src/Webpages/Listing/Listings.js
@@ -6,8 +6,10 @@ import Listing from "./Listing";
 import useAuth from "../../Components/Hooks/useAuth";
 import PageErrors from "../../Components/PageErrors/PageErrors";
 import Buttons from "./FilterButton";
+import FilterForm from "./FilterForm";
 function Listings({cartListings, setCartListings}) {
   const [allListings, setAllListings] = useState([]);
+  const [filteredListings, setFilteredListings] = useState([]);
   const [error, setError] = useState([]);
  // const type=[...new Set(data.map((listings) => listings.listingType))];
 
@@ -17,7 +19,10 @@ function Listings({cartListings, setCartListings}) {
   function getAllListings() {
     fetch("http://localhost:8080/api/listing")
       .then((response) => response.json())
-      .then((data) => setAllListings(data))
+      .then((data) => {
+        setAllListings(data);
+        setFilteredListings(data);
+      })
       .catch((errList) => {
         if (errList instanceof TypeError){
           setError(["Could not connect to api"])
@@ -27,18 +32,37 @@ function Listings({cartListings, setCartListings}) {
 
   useEffect(() => getAllListings(), []);
 
+  const filter = function(evt, filterObject) {
+    evt.preventDefault();
+    // expect an object with a bunch of booleans and ranges
+    // apply this object to allListings via maps?
+    console.log(filterObject);
+    let newFilteredListings = [...allListings]
+    if (filterObject.gameCheckbox) {
+      newFilteredListings = newFilteredListings.filter(l => l.listingType === "GAME")
+    }
+    setFilteredListings(newFilteredListings);
+  }
+
   return (
-    <div>
-      {/* <h4 className="col-12 text-center my-3 fw-bold">Filter</h4>
-      <Buttons  filterallListings={filterallListings}
-            setallListings={setallListings}
-            type={type}
-          /> */}
-      <PageErrors errors={error} />
-      {allListings.map((listing) => (
-        <Listing key={listing.id} listingData={listing} cartListings={cartListings} setCartListings={setCartListings} />
-      ))}
-    </div>
+    <>
+      <div> {
+      // this div should have a css property that makes it always to the left of the other div
+      }
+        <FilterForm filter={filter}/>
+      </div>
+      <div>
+        {/* <h4 className="col-12 text-center my-3 fw-bold">Filter</h4>
+        <Buttons  filterallListings={filterallListings}
+              setallListings={setallListings}
+              type={type}
+            /> */}
+        <PageErrors errors={error} />
+        {filteredListings.map((listing) => (
+          <Listing key={listing.id} listingData={listing} cartListings={cartListings} setCartListings={setCartListings} />
+        ))}
+      </div>
+    </>
   );
 }
 
