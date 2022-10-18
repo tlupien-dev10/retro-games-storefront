2b6f5237f2e726d6420d6c36bc6e7d4df8eb4ff5
Tim Lupien <tlupien@dev-10.com>
2022-10-18 15:30:55 -0500

    delete review (needs styling + improved err handling)

--- client/retro-games-storefront/src/Webpages/ListingId/Review.js
+++ client/retro-games-storefront/src/Webpages/ListingId/Review.js
@@ -1,12 +1,34 @@
 import ReviewForm from "./ReviewForm";
 import {useState} from "react";
+import useAuth from "../../Components/Hooks/useAuth";
 
 function Review({review, getListing}) {
 
     // needs an edit button only visible if author id = auth user id
     // needs delete button only visible for author id match + admin
+    const auth = useAuth();
 
     const [clicked, setClicked] = useState(false);
+    const [deleteClicked, setDeleteClicked] = useState(false);
+
+    const deleteIt = function() {
+        console.log(review.id)
+        fetch(`http://localhost:8080/api/review/${review.id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${auth.user.token}`,
+            }
+        })
+        .then(res => {
+            if (res.status === 204) {
+                console.log("success");
+                getListing();
+            } else {
+                return Promise.reject(res.json());
+            }
+        })
+        .catch(err => console.log(err));
+      };
 
     const setClickedFromSubmit = function() {
         setClicked(false);
@@ -23,6 +45,13 @@ function Review({review, getListing}) {
         {clicked ?
         <ReviewForm listingId={review.listing} clickFix={setClickedFromSubmit} startingReview={review}/> :
         <></>}
+        {!deleteClicked ?
+        <button type="button" onClick={() => setDeleteClicked(true)}>Delete</button> :
+        <>
+            <button type="button" onClick={deleteIt}>Yes</button>
+            <button type="button" onClick={() => setDeleteClicked(false)}>Cancel</button>
+        </>
+        }
         </>
     )
 }
